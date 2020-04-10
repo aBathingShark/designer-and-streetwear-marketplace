77f110288abd0e5ea6f424ce19c6eaaef0a30687
Tony <tony.truong38@gmail.com>
2020-04-09 23:22:46 -0400

    second commit

--- index.js
+++ index.js
@@ -147,6 +147,6 @@ app.get('/listing/:title', function (req, res) {
   res.render('post', listings);
 });
 
-app.listen(3000, function () {
-  console.log('Listening on port 3000!');
+app.listen(process.env.PORT || 3000, function() {
+  console.log('Listening!');
 });
